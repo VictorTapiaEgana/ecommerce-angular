c4d97718cab4d912b9eed8e70cd911c8832969a2
Victor Tapia <victortapiaegana@gmail.com>
2025-06-04 17:50:30 -0400

    feat: update CategoryList component to use mat-chip for category display and adjust HeroSection layout

--- src/components/ui/CategoryList/CategoryList.component.ts
+++ src/components/ui/CategoryList/CategoryList.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, signal } from '@angular/core';
 import { ProductService } from '../../../service/productService.service';
 import { environment } from '../../../environments/environment.development';
 import { CategoriaResponse } from '../../../types/categoriasResponseType';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-category-list',
-  imports: [],
+  imports: [MatChipsModule],
   templateUrl: './CategoryList.component.html',
   styles: ``,
 })
