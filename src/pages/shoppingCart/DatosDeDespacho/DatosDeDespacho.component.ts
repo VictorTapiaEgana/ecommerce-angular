24f9a3706b600230b8091b41fbbe47f0a2b134c2
Victor Tapia <victortapiaegana@gmail.com>
2025-06-10 18:33:06 -0400

    feat: agregar botones de cancelar y checkout en el formulario de datos de despacho, mejorar la estructura del código

--- src/pages/shoppingCart/DatosDeDespacho/DatosDeDespacho.component.ts
+++ src/pages/shoppingCart/DatosDeDespacho/DatosDeDespacho.component.ts
@@ -3,15 +3,11 @@ import { MatButton } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormField } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-datos-de-despacho',
-  imports: [MatButton, MatFormField,MatInputModule,FormsModule],
-  templateUrl: './DatosDeDespacho.component.html',
-  // styles: [`
-  //   input {
-  //     height:'5px' !important;
-  //   }
-  // `],
+  imports: [MatButton, MatFormField,MatInputModule,FormsModule,MatIcon],
+  templateUrl: './DatosDeDespacho.component.html',  
 })
 export class DatosDeDespachoComponent { }
