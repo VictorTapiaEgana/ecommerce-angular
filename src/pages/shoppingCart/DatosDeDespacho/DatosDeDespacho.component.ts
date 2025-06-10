ec012b189ee6ab7ddd5226e2679872ec019197d1
Victor Tapia <victortapiaegana@gmail.com>
2025-06-10 18:44:06 -0400

    feat: agregar botones para cancelar y limpiar carrito en el formulario de datos de despacho, mejorar la funcionalidad del carrito

--- src/pages/shoppingCart/DatosDeDespacho/DatosDeDespacho.component.ts
+++ src/pages/shoppingCart/DatosDeDespacho/DatosDeDespacho.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormField } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { CarritoDeCompraService } from '../../../service/CarritoDeCompra.service';
 
 @Component({
   selector: 'app-datos-de-despacho',
-  imports: [MatButton, MatFormField,MatInputModule,FormsModule,MatIcon],
+  imports: [MatButton, MatFormField,MatInputModule,FormsModule,MatIcon,RouterLink],
   templateUrl: './DatosDeDespacho.component.html',  
 })
-export class DatosDeDespachoComponent { }
+
+export class DatosDeDespachoComponent {
+
+  carrito = inject(CarritoDeCompraService)
+
+}
--- src/service/CarritoDeCompra.service.ts
+++ src/service/CarritoDeCompra.service.ts
@@ -38,4 +38,8 @@ export class CarritoDeCompraService {
      
   }
 
+  limpiarCarrito(){
+    this.ArrayCarritoDeCompras.set([])
+  }
+
 }
