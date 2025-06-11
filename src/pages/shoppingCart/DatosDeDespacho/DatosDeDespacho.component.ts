faed47e185a3e75f96ba0e22d860c7eb514a3f07
Victor Tapia <victortapiaegana@gmail.com>
2025-06-11 15:43:23 -0400

    feat: agregar validación de formulario en Datos de Despacho y mejorar la estructura del código

--- src/pages/shoppingCart/DatosDeDespacho/DatosDeDespacho.component.ts
+++ src/pages/shoppingCart/DatosDeDespacho/DatosDeDespacho.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule,FormBuilder, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormField } from '@angular/material/input';
@@ -6,15 +7,33 @@ import { FormsModule } from '@angular/forms';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { CarritoDeCompraService } from '../../../service/CarritoDeCompra.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-datos-de-despacho',
-  imports: [MatButton, MatFormField,MatInputModule,FormsModule,MatIcon,RouterLink],
+  imports: [ CommonModule, MatButton, MatFormField,MatInputModule,FormsModule,
+             MatIcon,RouterLink, ReactiveFormsModule],
   templateUrl: './DatosDeDespacho.component.html',  
 })
 
 export class DatosDeDespachoComponent {
 
   carrito = inject(CarritoDeCompraService)
+
+  fb = inject(FormBuilder)
+
+  formulario  = this.fb.group({
+      nombre:['',Validators.required],
+      apellidos:['',Validators.required],
+      email:['',[Validators.required, Validators.email]],
+      direccion:['',Validators.required],
+      ciudad:['',Validators.required]
+  })
+
+  ValidarDatos(){
+    
+    console.log(this.formulario.controls.nombre.errors)
+
+  }
 
 }
