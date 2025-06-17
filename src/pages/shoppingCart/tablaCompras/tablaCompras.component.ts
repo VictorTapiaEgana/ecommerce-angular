bf6114615c871bc131816aba46c51ad2fd4e977b
Victor Tapia <victortapiaegana@gmail.com>
2025-06-17 13:18:51 -0400

    feat: mejorar la interfaz del carrito y tabla de compras, añadiendo estilos y formateo de precios

--- src/pages/shoppingCart/tablaCompras/tablaCompras.component.ts
+++ src/pages/shoppingCart/tablaCompras/tablaCompras.component.ts
@@ -1,27 +1,20 @@
 import { Component, computed, input } from '@angular/core';
 import { ProductosCarritoType  } from '../../../types/productResponseType';
 
-
 @Component({
   selector: 'app-tabla-compras',
   imports: [],
   templateUrl: './tablaCompras.component.html',
   styles: ``,
 })
+
 export class TablaComprasComponent { 
 
   carrito = input.required<ProductosCarritoType[]>()
 
-  // total = computed (){
-  //   this.carrito()
-  //       .reduce((total ,prod) => total += prod.cantidad * prod.producto.price, 0 )
-  // }
-
-
-
   total = computed(()=> 
         this.carrito()
-          .reduce((total ,prod) => total += prod.cantidad * prod.producto.price, 0 )
+            .reduce((total ,prod) => total += prod.cantidad * prod.producto.price, 0 )
   )
   
 }
